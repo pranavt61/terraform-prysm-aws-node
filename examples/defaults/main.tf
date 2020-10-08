2d22b4ebedf9121a988721b63592c91d754c3e2a
rob <robcannonxyz@gmail.com>
2020-10-07 19:29:09 -0700

    fixing example for tests

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -9,12 +9,20 @@ provider "aws" {
 variable "private_key_path" {}
 variable "public_key_path" {}
 
+//variable "keystore_path" {
+//  default = ""
+//}
+//locals {
+//  keystore_path = var.keystore_path == "" ? "${path.module}/../../test/fixtures/keystores/keystore-default" : var.keystore_path
+//}
+
 module "defaults" {
   source            = "../.."
-  keystore_password = ""
   network_name      = "medalla"
   private_key_path  = var.private_key_path
   public_key_path   = var.public_key_path
+  keystore_password = "ItWorks!!!!1"
+  keystore_path     = "${path.module}/../../test/fixtures/keystores/keystore-default"
 }
 
 output "public_ip" {
